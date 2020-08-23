afe5aa18c53d6f92a76d93860c92772b0df5e32e
cappydh <ahmeturaloglu@msn.com>
2020-08-23 15:40:02 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const CardView = ({ items }) => {
+  const navigations = items
+
+  const [itemLink, setItemLink] = useState(navigations[0].image)
+  const [selectedItem, setSelectedItem] = useState(navigations[0])
+
+  const handleClick = (item) => {
+    setItemLink(item.image)
+    setSelectedItem(item)
+  }
+
+  const renderNav = (items) => {
+    return items.map((item) => {
+      return (
+        <li key={item.id}>
+          <div
+            className={
+              selectedItem.id === item.id
+                ? styles.item + ' ' + styles.activeAttr
+                : styles.item
+            }
+            onClick={() => handleClick(item)}
+          >
+            <div
+              className={
+                selectedItem.id === item.id
+                  ? styles.activeSelectorActive
+                  : styles.activeSelector
+              }
+            ></div>
+            <i className={styles.arrowDown}></i>
+            <p className={styles.itemHeader}>{item.header}</p>
+            <p
+              className={
+                selectedItem.id === item.id
+                  ? styles.activeItemDescription
+                  : styles.itemDescription
+              }
+            >
+              {item.description}
+            </p>
+          </div>
+        </li>
+      )
+    })
+  }
+
+  return (
+    <div className={styles.attrSection}>
+      <div className={styles.attrImageSection}>
+        <img src={itemLink} className={styles.attrImage} alt='logo' />
+      </div>
+      <div className={styles.attrListSection}>
+        <div className={styles.attributeList}>
+          <ul>{renderNav(navigations)}</ul>
+        </div>
+      </div>
+    </div>
+  )
 }
