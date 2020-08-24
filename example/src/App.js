0e7bcb7acc35ace2c68ca91a36d62201896bf72b
cappydh <ahmeturaloglu@msn.com>
2020-08-24 22:04:08 +0300

    added tests

--- example/src/App.js
+++ example/src/App.js
@@ -11,14 +11,14 @@ const App = () => {
       description:
         'dolor sit amet, consectetur adipiscing elit. Sed tempus nunc et tincidunt lobortis. Aliquam placerat, justo sit amet mattis molestie, ipsum nisi congue turpis, in imperdiet nisi nisl sit amet arcu. Donec euismod eu ante quis elementum. Maecenas commodo erat',
       image:
-        'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+        'https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
     },
     {
       id: 2,
       header: 'Sed cursus',
       description:
         'in metus quis tempor. Donec at venenatis magna, vel fringilla dui. Curabitur id gravida ipsum. Donec at mollis massa. Nullam metus elit, pret',
-      image: 'https://cerberus-laboratories.com/images/blog/random_numbers.jpg'
+      image: 'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
     },
     {
       id: 3,
@@ -26,14 +26,14 @@ const App = () => {
       description:
         'condimentum purus, non sagittis massa faucibus id. Sed finibus convallis lectus eu fringilla. Proin lacinia sem vitae nunc consectetur, a faucibus orci ultricie',
       image:
-        'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+        'https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=518&q=80'
     },
     {
       id: 4,
       header: 'Proin et urna',
       description:
         'vitae neque fermentum fringilla. Proin bibendum sollicitudin aliquet. Fusce id magna aliquam, pulvinar metus vitae, bibendum felis.',
-      image: 'https://cerberus-laboratories.com/images/blog/random_numbers.jpg'
+      image: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80'
     },
     {
       id: 5,
@@ -41,15 +41,15 @@ const App = () => {
       description:
         'finibus id eros eu, commodo sollicitudin lacus. Nunc semper enim nec est viverra, at pharetra orci lobortis. Nulla facilisi. Sed non lectus nunc.',
       image:
-        'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+        'https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=663&q=80'
     }
   ]
   return (
     <CardView
       items={items}
       activeColor={'#000'}
-      imageHeight={'650px'}
-      imageWidth={'800px'}
+      imageHeight={'600px'}
+      imageWidth={''}
     />
   )
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,62 @@
-import { ExampleComponent } from '.'
+import { CardView } from '.'
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure  } from 'enzyme'
 
-describe('ExampleComponent', () => {
+const items = [
+  {
+    id: 1,
+    header: 'Lorem ipsum',
+    description:
+      'dolor sit amet',
+    image:
+      'https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
+  },
+  {
+    id: 2,
+    header: 'Sed cursus',
+    description:
+      'in metus quis tempor',
+    image: 'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+  }
+]
+
+configure({ adapter: new Adapter() })
+
+describe('CardView', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(CardView).toBeTruthy()
+  })
+  it('render items correctly', () => {
+    const wrapper = shallow(<CardView items={items} />)
+    const list = wrapper.find('li')
+    expect(list.at(0).key()).toEqual('1')
+    expect(list.at(1).key()).toEqual('2')
+  })
+  it('render headers', () => {
+    const wrapper = shallow(<CardView items={items} />)
+    expect(wrapper.find('.itemHeader')).toHaveLength(2)
+  })
+  it('render only first description', () => {
+    const wrapper = shallow(<CardView items={items} />)
+    const descriptionFirst = items[0].description
+    expect(wrapper.find('.itemDescription')).toHaveLength(1)
+    expect(wrapper.find('.activeItemDescription').text()).toEqual(descriptionFirst)
+  })
+  it('changes img src on click', () => {
+    const wrapper = shallow(<CardView items={items} />)
+    const listItem = wrapper.find('.item').at(1)
+    const secondImage = items[1].image
+    listItem.simulate('click')
+    const imageSrc = wrapper.find('img').prop('src')
+    expect(imageSrc).toEqual(secondImage)
+  })
+  it('changes description on click', () => {
+    const wrapper = shallow(<CardView items={items} />)
+    const listItem = wrapper.find('.item').at(1)
+    const descriptionSecond = items[1].description
+    listItem.simulate('click')
+    const activeItemDescription = wrapper.find('.activeItemDescription').text()
+    expect(activeItemDescription).toEqual(descriptionSecond)
   })
 })
